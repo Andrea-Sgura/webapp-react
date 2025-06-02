6125b4338d952cd0c88ccdaa5ac6877dbe5eb142
Andrea-Sgura <andreasgura96@gmail.com>
2025-06-03 01:53:06 +0200

    [UPD] Creato Layout statico nella Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,7 +4,86 @@ const HomePage = () => {
     return (
         <>
             <h1 className="text-secondary">Movies</h1>
-            <div className="row"></div>
+            <div className="row gy-4">
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/450/300" className="img-fluid" alt="Movie" />
+                        </div>
+                        <div className="card-body">
+                            <h3 className="text-secondary">Titolo</h3>
+                            <h4><em>Autore</em></h4>
+                            <p>Trama</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/450/300" className="img-fluid" alt="Movie" />
+                        </div>
+                        <div className="card-body">
+                            <h3 className="text-secondary">Titolo</h3>
+                            <h4><em>Autore</em></h4>
+                            <p>Trama</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/450/300" className="img-fluid" alt="Movie" />
+                        </div>
+                        <div className="card-body">
+                            <h3 className="text-secondary">Titolo</h3>
+                            <h4><em>Autore</em></h4>
+                            <p>Trama</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/450/300" className="img-fluid" alt="Movie" />
+                        </div>
+                        <div className="card-body">
+                            <h3 className="text-secondary">Titolo</h3>
+                            <h4><em>Autore</em></h4>
+                            <p>Trama</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/450/300" className="img-fluid" alt="Movie" />
+                        </div>
+                        <div className="card-body">
+                            <h3 className="text-secondary">Titolo</h3>
+                            <h4><em>Autore</em></h4>
+                            <p>Trama</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 col-lg-4">
+                    <div className="card">
+                        <div className="card-image-top">
+                            <img src="https://picsum.photos/450/300" className="img-fluid" alt="Movie" />
+                        </div>
+                        <div className="card-body">
+                            <h3 className="text-secondary">Titolo</h3>
+                            <h4><em>Autore</em></h4>
+                            <p>Trama</p>
+                            <a href="#">Link</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
