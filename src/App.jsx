59b0baaa974792f698b1ea87ced8ae5d32f87e52
Andrea-Sgura <andreasgura96@gmail.com>
2025-06-03 01:24:58 +0200

    [UPD] Creata componente Homepage + importata in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import DefaultLayout from "./layouts/DefaultLayout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<DefaultLayout />}>
-          <Route index element={<h1>HomePage</h1>}></Route>
+          <Route index element={<HomePage />}></Route>
+          <Route path="/movies/:id" element={<h1>Dettaglio Pagina</h1>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <>
+            <h1 className="text-secondary">Movies</h1>
+            <div className="row"></div>
+        </>
+    )
+}
+
+export default HomePage
